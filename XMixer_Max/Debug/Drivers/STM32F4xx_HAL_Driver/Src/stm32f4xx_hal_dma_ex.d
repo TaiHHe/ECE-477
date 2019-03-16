38552ecd7e40dcda54e8fc40747d6314f8543abd
Taihai He <taihhe@gmail.com>
2019-03-16 00:07:19 -0400

    Finished the basic sturcture of MCU program.

---
+++ XMixer_Max/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.d
@@ -0,0 +1,95 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+C:/Users/eepc19adm/Desktop/Workspace/XMixer_Max/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
---
+++ XMixer_Max/Src/i2c-lcd.c
@@ -0,0 +1,65 @@
+
+/** Put this in the src folder **/
+
+#include "i2c-lcd.h"
+extern I2C_HandleTypeDef hi2c1;  // change your handler here accordingly
+
+#define SLAVE_ADDRESS_LCD 0x4E // change this according to ur setup
+
+void lcd_send_cmd (char cmd)
+{
+  char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (cmd&0xf0);
+	data_l = ((cmd<<4)&0xf0);
+	data_t[0] = data_u|0x0C;  //en=1, rs=0
+	data_t[1] = data_u|0x08;  //en=0, rs=0
+	data_t[2] = data_l|0x0C;  //en=1, rs=0
+	data_t[3] = data_l|0x08;  //en=0, rs=0
+	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, 100);
+}
+
+void lcd_send_data (char data)
+{
+	char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (data&0xf0);
+	data_l = ((data<<4)&0xf0);
+	data_t[0] = data_u|0x0D;  //en=1, rs=0
+	data_t[1] = data_u|0x09;  //en=0, rs=0
+	data_t[2] = data_l|0x0D;  //en=1, rs=0
+	data_t[3] = data_l|0x09;  //en=0, rs=0
+	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, 100);
+}
+
+//void lcd_init (void)
+//{
+//	lcd_send_cmd (0x02);
+//	lcd_send_cmd (0x28);
+//	lcd_send_cmd (0x0c);
+//	lcd_send_cmd (0x80);
+//}
+
+void lcd_init (void)
+{
+	uint8_t i=0;
+	HAL_Delay(100);
+	for(i=0;i<3;i++)//sending 3 times: select 4-bit mode
+	{
+		lcd_send_cmd(0x03);
+		HAL_Delay(45);
+	}
+	lcd_send_cmd (0x02);
+	HAL_Delay(100);
+	lcd_send_cmd (0x28);
+	HAL_Delay(1);
+	lcd_send_cmd (0x0c);
+	HAL_Delay(1);
+	lcd_send_cmd (0x80);
+	HAL_Delay(1);
+}
+
+void lcd_send_string (char *str)
+{
+	while (*str) lcd_send_data (*str++);
+}
